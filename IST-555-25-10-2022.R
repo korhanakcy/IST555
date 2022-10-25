8d9d82d958e36541b45dd6439288ad88bedd2ca6
Korhan <74705516+korhanakcy@users.noreply.github.com>
2022-10-25 10:34:30 +0300

    Add files via upload

---
+++ IST-555-25-10-2022.R
@@ -0,0 +1,15 @@
+#IS
+#R İLE VERİ BİLİMİ 
+ #140 ile 195 arasından 1000 adet veri üretmek
+x <- sample(140:195, 1000, replace = T)
+
+ #male(m) ve female(f)'den 1000 adet veri üretmek
+y <- sample(c("m","f"), 1000, replace = T)
+
+#dataframe oluşturma
+data <- data.frame(y,x)
+
+#sütun isimlerinin değişimi
+colnames(data) <- c("height","gender")
+
+data

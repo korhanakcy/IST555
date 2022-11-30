aa51c7892e888a92c0e08edfcb83107ed45c0775
Korhan <74705516+korhanakcy@users.noreply.github.com>
2022-11-30 11:32:34 +0300

    Add files via upload

--- regresyon.R
+++ regresyon.R
@@ -25,3 +25,22 @@ for (i in 1:1000){
   sigma = sigma + ((weight[i]-yhat[i])**2/998)
 }
 sqrt(sigma)
+
+#other formula
+yi_y_hat <- (weight-yhat)
+yi_yhat_square <- yi_y_hat**2
+sum_yi_yhat_square <- sum(yi_yhat_square)
+sigma <- sum_yi_yhat_square/998
+sqrt(sigma)
+
+#r2
+y_hat_y_i <- (yhat-mean(weight))
+y_hat_y_i_sq <- (y_hat_y_i)**2
+
+y_i_y_mean <- (weight - mean(weight))
+y_i_y_mean_sq <- (y_i_y_mean)**2
+
+r2 <- (sum(y_hat_y_i_sq)/sum(y_i_y_mean_sq))
+r2
+
+#

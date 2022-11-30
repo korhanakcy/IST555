3f913b5c71cb20bc255a5bdffc4a70aa110841a8
Korhan <74705516+korhanakcy@users.noreply.github.com>
2022-11-30 10:51:16 +0300

    Add files via upload

--- regresyon.R
+++ regresyon.R
@@ -5,13 +5,13 @@ x <- sample(140:195, 1000, replace = T)
 set.seed(99)
 weight <- sample(45:100, 1000, replace = T)
 
-lm(x ~ weight)
+lm(weight ~ x)
 
 aa <- sample(c(1), 1000, replace = T)
 a <- data.frame(aa,x)
 a <- as.matrix(a)
 
-hatb <- solve(t(a)%*%a)%*%t(a)%*%data$height
+hatb <- solve(t(a)%*%a)%*%t(a)%*%weight
 rownames(hatb) <- c("b0","b1")
 colnames(hatb) <- c("X")
 hatb

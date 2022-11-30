9eacd62858857db2fe738a8f4f81e2c2496112a0
Korhan <74705516+korhanakcy@users.noreply.github.com>
2022-11-30 11:09:27 +0300

    Add files via upload

--- regresyon.R
+++ regresyon.R
@@ -5,13 +5,23 @@ x <- sample(140:195, 1000, replace = T)
 set.seed(99)
 weight <- sample(45:100, 1000, replace = T)
 
-lm(weight ~ x)
+model <- summary(lm(weight ~ x))
+model
 
-aa <- sample(c(1), 1000, replace = T)
-a <- data.frame(aa,x)
-a <- as.matrix(a)
+X <- data.frame(rep(1,1000),x)
+X <- as.matrix(X)
 
-hatb <- solve(t(a)%*%a)%*%t(a)%*%weight
+
+hatb <- solve(t(X)%*%X)%*%t(X)%*%weight
 rownames(hatb) <- c("b0","b1")
 colnames(hatb) <- c("X")
 hatb
+
+yhat <- fitted.values(lm(weight ~ x))
+
+#residuals
+sigma=0
+for (i in 1:1000){
+  sigma = sigma + ((weight[i]-yhat[i])**2/999)
+}
+sqrt(sigma)
